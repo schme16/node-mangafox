6176d8dac7070c9aa21e28971cf2a2314998501f
Ankirama <fabien.martinez@epitech.eu>
2018-01-26 22:37:55 +0100

    [Search]: Add image field in the result

--- main.js
+++ main.js
@@ -74,7 +74,8 @@ mangaFox.search = function(manga, callback) {
         //console.log(d.find('#mangalist'), mangaFoxBaseURI + '/search.php?name_method=cw&name=' + encodeURIComponent(manga) + '&type=&author_method=cw&author=&artist_method=cw&artist=&genres%5BAction%5D=0&genres%5BAdult%5D=0&genres%5BAdventure%5D=0&genres%5BComedy%5D=0&genres%5BDoujinshi%5D=0&genres%5BDrama%5D=0&genres%5BEcchi%5D=0&genres%5BFantasy%5D=0&genres%5BGender+Bender%5D=0&genres%5BHarem%5D=0&genres%5BHistorical%5D=0&genres%5BHorror%5D=0&genres%5BJosei%5D=0&genres%5BMartial+Arts%5D=0&genres%5BMature%5D=0&genres%5BMecha%5D=0&genres%5BMystery%5D=0&genres%5BOne+Shot%5D=0&genres%5BPsychological%5D=0&genres%5BRomance%5D=0&genres%5BSchool+Life%5D=0&genres%5BSci-fi%5D=0&genres%5BSeinen%5D=0&genres%5BShoujo%5D=0&genres%5BShoujo+Ai%5D=0&genres%5BShounen%5D=0&genres%5BShounen+Ai%5D=0&genres%5BSlice+of+Life%5D=0&genres%5BSmut%5D=0&genres%5BSports%5D=0&genres%5BSupernatural%5D=0&genres%5BTragedy%5D=0&genres%5BWebtoons%5D=0&genres%5BYaoi%5D=0&genres%5BYuri%5D=0&released_method=eq&released=&rating_method=eq&rating=&is_completed=&advopts=1')
         d.find('#mangalist > ul li').each(function(i, e) {
             var b = $(e).find('.title');
-            list[b.text()] = { id: b.attr('rel'), title: b.text()};
+            var img = $(e).find('.manga_img img');
+            list[b.text()] = { id: b.attr('rel'), title: b.text(), img: img.attr('src') };
         });
         callback(list);
     }, true);
